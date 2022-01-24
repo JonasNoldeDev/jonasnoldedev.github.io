0b059bfa17b4e948f31cc6c21ecdefe7bb360c19
Jonas Nolde <jonasnoldedev@gmail.com>
2022-01-24 19:22:21 +0100

    Add new build

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+// service worker
+var currentCacheVersion = 'codi-lernapp-v1';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(currentCacheVersion).then(function (cache) {
+            return cache.addAll([
+                '/',
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});
+self.addEventListener('activate', function (event) {
+    var cacheWhitelist = [currentCacheVersion];
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
